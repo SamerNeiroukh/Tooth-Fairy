fde94aa0dd4645c60ce61422ed14b4e84a6e6f74
Dvir Yotvat <72920475+MrRabbiT-coder@users.noreply.github.com>
2021-05-20 15:18:59 +0300

    Update booking.js

--- ToothFairy web/js/booking.js
+++ ToothFairy web/js/booking.js
@@ -2,3 +2,29 @@
 // function book_date() {
 //   document.getElementById("bookdate").step = "9";
 // }
+
+
+// make a contact
+function csubmitForm() {
+    // initiate variables with form content
+    var name = $("#cname").val();
+    var emailUser = $("#cemail").val();
+    var message = $("#cmessage").val();
+    var terms = $("#cterms").val();
+    var toothEmail = "my.tooth.fairy0@gmail.com";
+    //var ander = "מ%0D" + emailUser + message
+  
+    // *********** //
+    window.open(
+      "mailto:my.tooth.fairy0@gmail.com?subject=יצירת קשר  - " +
+        name +
+        "&body=מייל לחזרה: " +
+        emailUser +
+        "%0d" +
+        "%0d" +
+        message +
+        "%0d"
+    );
+  
+    // *********** //
+  }
