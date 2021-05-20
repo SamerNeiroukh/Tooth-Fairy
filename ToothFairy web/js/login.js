a01c209c623d8a486707b94fad2c234aee775c42
Dvir Yotvat <72920475+MrRabbiT-coder@users.noreply.github.com>
2021-05-20 12:12:23 +0300

    Update login.js

--- ToothFairy web/js/login.js
+++ ToothFairy web/js/login.js
@@ -1,19 +1,19 @@
-const { default: firebase } = require("./firebase");
+// const { default: firebase } = require("./firebase");
 
-var firebaseConfig = {
-    apiKey: "AIzaSyD5B3hTjXXygIi62Sb9pyiolRAaR7moR0E",
-    authDomain: "tooth-fairy-web.firebaseapp.com",
-    databaseURL: "https://tooth-fairy-web-default-rtdb.firebaseio.com",
-    projectId: "tooth-fairy-web",
-    storageBucket: "tooth-fairy-web.appspot.com",
-    messagingSenderId: "473402834454",
-    appId: "1:473402834454:web:511aea6ff025a2c5817f37",
-    measurementId: "G-3KF2SC8288"
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyC9_qCGYu6YdTVNCxGHnV8T0SETnGyo8Qs",
+    authDomain: "toothfairyweb-6be63.firebaseapp.com",
+    projectId: "toothfairyweb-6be63",
+    storageBucket: "toothfairyweb-6be63.appspot.com",
+    messagingSenderId: "854109357498",
+    appId: "1:854109357498:web:e25bc99d544db5b31bc983",
+    measurementId: "G-GTMX6JLZCJ"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
-
 
 
 
@@ -26,6 +26,10 @@ var firebaseConfig = {
 //     }
 //   });
 
+function send_mail(){
+  window.open('mailto:dvir563@gmail.com?subject=test&body=test');
+}
+
   function login(){
     var userEmail = document.getElementById("lemail").value;
     var userPass = document.getElementById("lpass").value;
@@ -33,7 +37,7 @@ var firebaseConfig = {
     firebase.auth().signInWithEmailAndPassword(userEmail, userPass)
     .then(function(firebaseUser) {
         // alert("user logd in 2")
-        // window.location.href = "booking.html"
+        window.location.href = "booking.html"
 
       ffirebase.auth().signup
 
