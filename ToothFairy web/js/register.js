1110efce047df00529ebc343eafc2ca2e0374ee8
Dvir Yotvat <72920475+MrRabbiT-coder@users.noreply.github.com>
2021-05-20 14:49:19 +0300

    Update register.js

--- ToothFairy web/js/register.js
+++ ToothFairy web/js/register.js
@@ -1,17 +1,22 @@
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-    apiKey: "AIzaSyC9_qCGYu6YdTVNCxGHnV8T0SETnGyo8Qs",
-    authDomain: "toothfairyweb-6be63.firebaseapp.com",
-    projectId: "toothfairyweb-6be63",
-    storageBucket: "toothfairyweb-6be63.appspot.com",
-    messagingSenderId: "854109357498",
-    appId: "1:854109357498:web:e25bc99d544db5b31bc983",
-    measurementId: "G-GTMX6JLZCJ"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyC9_qCGYu6YdTVNCxGHnV8T0SETnGyo8Qs",
+  authDomain: "toothfairyweb-6be63.firebaseapp.com",
+  projectId: "toothfairyweb-6be63",
+  storageBucket: "toothfairyweb-6be63.appspot.com",
+  messagingSenderId: "854109357498",
+  appId: "1:854109357498:web:e25bc99d544db5b31bc983",
+  measurementId: "G-GTMX6JLZCJ",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+$(document).ready(function () {
+  var x = document.getElementById("user_exsist");
+  x.style.display = "none";
+});
 
 function reg() {
   let userEmail = document.getElementById("remail").value;
@@ -38,20 +43,31 @@ function reg() {
     .signInWithEmailAndPassword(userEmail, userPass)
     .then(function (firebaseUser) {
       alert("user already exsist");
-      return
+
+
+        var x = document.getElementById("user_exsist");
+        x.style.display = "block";
+      return;
     });
 
   // make a new user
-  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass)
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(userEmail, userPass)
     .then((userCredential) => {
       // Signed in
       var user = userCredential.user;
       alert("secsses to make user");
-      window.location.href = "index.html"
-      })
+      window.location.href = "index.html";
+    })
     .catch((error) => {
       var errorCode = error.code;
       var errorMessage = error.message;
       // ..
     });
+}
+
+function log()
+{
+  window.location.href = "login.html";
 }
