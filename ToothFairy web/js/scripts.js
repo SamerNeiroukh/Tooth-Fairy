045ee31e7898b880fdc5f9aa277b8e71622acf2e
Dvir Yotvat <72920475+MrRabbiT-coder@users.noreply.github.com>
2021-05-20 15:19:15 +0300

    Update scripts.js

--- ToothFairy web/js/scripts.js
+++ ToothFairy web/js/scripts.js
@@ -260,48 +260,30 @@
         }
     });
 
-    function csubmitForm() {
-        // initiate variables with form content
-		var name = $("#cname").val();
-		var email = $("#cemail").val();
-        var message = $("#cmessage").val();
-        var terms = $("#cterms").val();
-        $.ajax({
-            type: "POST",
-            url: "php/contactform-process.php",
-            data: "name=" + name + "&email=" + email + "&message=" + message + "&terms=" + terms, 
-            success: function(text) {
-                if (text == "success") {
-                    cformSuccess();
-                } else {
-                    cformError();
-                    csubmitMSG(false, text);
-                }
-            }
-        });
-	}
-
-    function cformSuccess() {
-        $("#contactForm")[0].reset();
-        csubmitMSG(true, "Message Submitted!");
-        $("input").removeClass('notEmpty'); // resets the field label after submission
-        $("textarea").removeClass('notEmpty'); // resets the field label after submission
-    }
-
-    function cformError() {
-        $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-            $(this).removeClass();
-        });
-	}
-
-    function csubmitMSG(valid, msg) {
-        if (valid) {
-            var msgClasses = "h3 text-center tada animated";
-        } else {
-            var msgClasses = "h3 text-center";
-        }
-        $("#cmsgSubmit").removeClass().addClass(msgClasses).text(msg);
-    }
+// make a contact
+function csubmitForm() {
+    // initiate variables with form content
+    var name = $("#cname").val();
+    var emailUser = $("#cemail").val();
+    var message = $("#cmessage").val();
+    var terms = $("#cterms").val();
+    var toothEmail = "my.tooth.fairy0@gmail.com";
+    //var ander = "מ%0D" + emailUser + message
+  
+    // *********** //
+    window.open(
+      "mailto:my.tooth.fairy0@gmail.com?subject=יצירת קשר  - " +
+        name +
+        "&body=מייל לחזרה: " +
+        emailUser +
+        "%0d" +
+        "%0d" +
+        message +
+        "%0d"
+    );
+  
+    // *********** //
+  }
 
 
     /* Privacy Form */
