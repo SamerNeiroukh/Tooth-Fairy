ffa653550a0a526919f8caaabb760bb362e4758c
Dvir Yotvat <DVIR563@GMAIL.COM>
2021-05-20 18:45:02 +0300

    remove connect

--- ToothFairy web/js/login.js
+++ ToothFairy web/js/login.js
@@ -82,27 +82,3 @@ function forgotpass() {
 }
 
 
-// make a contact
-function csubmitForm() {
-  // initiate variables with form content
-  var name = $("#cname").val();
-  var emailUser = $("#cemail").val();
-  var message = $("#cmessage").val();
-  var terms = $("#cterms").val();
-  var toothEmail = "my.tooth.fairy0@gmail.com";
-  //var ander = "מ%0D" + emailUser + message
-
-  // *********** //
-  window.open(
-    "mailto:my.tooth.fairy0@gmail.com?subject=יצירת קשר  - " +
-      name +
-      "&body=מייל לחזרה: " +
-      emailUser +
-      "%0d" +
-      "%0d" +
-      message +
-      "%0d"
-  );
-
-  // *********** //
-}
--- ToothFairy web/js/register.js
+++ ToothFairy web/js/register.js
@@ -72,28 +72,3 @@ function log()
   window.location.href = "login.html";
 }
 
-
-// make a contact
-function csubmitForm() {
-  // initiate variables with form content
-  var name = $("#cname").val();
-  var emailUser = $("#cemail").val();
-  var message = $("#cmessage").val();
-  var terms = $("#cterms").val();
-  var toothEmail = "my.tooth.fairy0@gmail.com";
-  //var ander = "מ%0D" + emailUser + message
-
-  // *********** //
-  window.open(
-    "mailto:my.tooth.fairy0@gmail.com?subject=יצירת קשר  - " +
-      name +
-      "&body=מייל לחזרה: " +
-      emailUser +
-      "%0d" +
-      "%0d" +
-      message +
-      "%0d"
-  );
-
-  // *********** //
-}
