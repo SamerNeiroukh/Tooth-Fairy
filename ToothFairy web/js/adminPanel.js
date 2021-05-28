c2017d36ee4407352c1507822746f6f7a6bd1d60
Dvir Yotvat <DVIR563@GMAIL.COM>
2021-05-28 14:23:49 +0300

    add stories menege

--- ToothFairy web/js/adminPanel.js
+++ ToothFairy web/js/adminPanel.js
@@ -24,118 +24,115 @@ firebase.analytics();
 
 // ******************************
 
-$( document ).ready(function() {
+$(document).ready(function () {
   $("#manageSW").hide();
   $("#img_manage").hide();
   $("#manageStories").hide();
   $("#manage_appointments").hide();
-  $("welcome").show();
+  $("#welcome").show();
 });
-
-
-// function welcomeAdmin() {
-//   $("#manageSW").hide();
-//   $("#img_manage").hide();
-//   $("#manageStories").hide();
-//   $("#manage_appointments").hide();
-//   $("welcome").show();
-// }
 
 function manageGallery() {
   $("#manageStories").hide();
   $("#manageSW").hide();
   $("#manage_appointments").hide();
-  $("welcome").hide();
+  $("#welcome").hide();
   $("#img_manage").show();
 }
 
-  let ImgName, ImgUrl;
-  let files = [];
-  let reader;
+let ImgName, ImgUrl;
+let files = [];
+let reader;
 
-  // select image from pc
-  document.getElementById("select").onclick = function (e) {
-    let input = document.createElement("input");
-    input.type = "file";
+// select image from pc
+document.getElementById("select").onclick = function (e) {
+  let input = document.createElement("input");
+  input.type = "file";
 
-    input.onchange = (e) => {
-      files = e.target.files;
-      reader = new FileReader();
-      reader.onload = function () {
-        document.getElementById("myimg").src = reader.result;
-      };
-      reader.readAsDataURL(files[0]);
+  input.onchange = (e) => {
+    files = e.target.files;
+    reader = new FileReader();
+    reader.onload = function () {
+      document.getElementById("myimg").src = reader.result;
     };
-    input.click();
+    reader.readAsDataURL(files[0]);
   };
+  input.click();
+};
 
-  // upload image to the firebase storege
-  document.getElementById("upload").onclick = function () {
-    // case not selected name for the image return
-    if (!$("#namebox").val()) {
-      alert("נא לבחור שם לתמונה");
-      return;
-    }
-
-    // get the image from the text box
-    ImgName = document.getElementById("namebox").value;
-    let uploadTask = firebase
-      .storage()
-      .ref("Images/" + ImgName + ".png")
-      .put(files[0]);
-
-    // upload the image to the firebase storege
-    uploadTask.on(
-      "state_changed",
-      function (snapshot) {
-        let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        document.getElementById("UpProgress").innerHTML =
-          "Upload " + progress + "%";
-      },
-
-      function (error) {
-        alert("error to upload img");
-      },
-
-      // upload the image ditails to firebase database of the image
-      function () {
-        uploadTask.snapshot.ref.getDownloadURL().then(function (url) {
-          ImgUrl = url;
-
-          firebase
-            .database()
-            .ref("Pictures/" + ImgName)
-            .set({
-              Name: ImgName,
-              Link: ImgUrl,
-            });
-          alert("תמונה עלתה בהצלחה");
-        });
-      }
-    );
-  };
-
-  // get all images from the firebase
-  function getImagesToDelete() {
-    let rootref = firebase.database().ref().child("Pictures");
-
-    rootref.on("child_added", (snap) => {
-      let image = snap.child("Link").val();
-      let imageName = snap.child("Name").val();
-
-      let str = 
-        `<a> בבקשה לחץ בבקשה על התמונה שברצונך למחוק</a> <div> <button id="${imageName}"` +
-        "onclick=deleteImg('" +
-        imageName +
-        "')" +
-        `> <img src= ${image} style="width:100px" style="height:100px"></img> </button></div>`;
-
-      $("#delete_imgs").append(str);
-    });
+// upload image to the firebase storege
+document.getElementById("upload").onclick = function () {
+  // case not selected name for the image return
+  if (!$("#namebox").val()) {
+    alert("נא לבחור שם לתמונה");
+    return;
   }
 
-  // delete images from firebase storage and data of image from firebase realtime database
-  function deleteImg(imageName) {
+  // get the image from the text box
+  ImgName = document.getElementById("namebox").value;
+  let uploadTask = firebase
+    .storage()
+    .ref("Images/" + ImgName + ".png")
+    .put(files[0]);
+
+  // upload the image to the firebase storege
+  uploadTask.on(
+    "state_changed",
+    function (snapshot) {
+      let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      document.getElementById("UpProgress").innerHTML =
+        "Upload " + progress + "%";
+    },
+
+    function (error) {
+      alert("error to upload img");
+    },
+
+    // upload the image ditails to firebase database of the image
+    function () {
+      uploadTask.snapshot.ref.getDownloadURL().then(function (url) {
+        ImgUrl = url;
+
+        firebase
+          .database()
+          .ref("Pictures/" + ImgName)
+          .set({
+            Name: ImgName,
+            Link: ImgUrl,
+          });
+        alert("תמונה עלתה בהצלחה");
+      });
+    }
+  );
+};
+
+// get all images from the firebase and show the admin the images to delete
+function getImagesToDelete() {
+  // reset div
+  let div = document.getElementById("delete_imgs");
+  while (div.firstChild) {
+    div.removeChild(div.firstChild);
+  }
+  // run on all the data in the realtime database in filde Pictures
+  let rootref = firebase.database().ref().child("Pictures");
+  rootref.on("child_added", (snap) => {
+    let image = snap.child("Link").val();
+    let imageName = snap.child("Name").val();
+
+    let str =
+      `<div> <button id="${imageName}"` +
+      "onclick=deleteImg('" +
+      imageName +
+      "')" +
+      `> <img src= ${image} style="width:100px" style="height:100px"></img> </button></div>`;
+
+    $("#delete_imgs").append(str);
+  });
+}
+
+// delete images from firebase storage and data of image from firebase realtime database
+function deleteImg(imageName) {
+  if (confirm("האם אתה בטוח שברצונך למחוק תמונה זו?")) {
     // delete image from firebase storage
     firebase
       .storage()
@@ -152,8 +149,10 @@ function manageGallery() {
     }
     // call back to all images
     getImagesToDelete();
+  } else {
+    return;
   }
-
+}
 
 // manage appointments
 function manageAppo() {
@@ -182,6 +181,75 @@ function managePersonalStories() {
   $("#manage_appointments").hide();
   $("#manageStories").show();
 }
+
+// add story to realtime database firebase
+function up_story() {
+  if (!$("#headline").val()) {
+    alert("נא לבחור כותרת");
+    return;
+  } else if (!$("#story_msg").val()) {
+    alert("נא להוסיף מלל");
+    return;
+  }
+
+  let title = $("#headline").val();
+  let story = $("#story_msg").val();
+
+  // add story to realtime database firebase
+  firebase
+    .database()
+    .ref("Stories/" + title)
+    .set({
+      story_title: title,
+      story_content: story,
+    });
+
+  alert("סיפור אישי עלה בהצלחה");
+
+  // reset values of the textbox
+  $("#headline").val("");
+  $("#story_msg").val("");
+}
+
+// get all Stories from the firebase and show the admin the Stories to delete
+function getStoriesToDelete() {
+  // reset div
+  let div = document.getElementById("delete_stories");
+  while (div.firstChild) {
+    div.removeChild(div.firstChild);
+  }
+  // run on all the data in the realtime database in filde Pictures
+  let rootref = firebase.database().ref().child("Stories");
+  rootref.on("child_added", (snap) => {
+    let title = snap.child("story_title").val();
+    let story = snap.child("story_content").val();
+
+    let str =
+      `<div> <button id="${title}"` +
+      "onclick=delete_story('" +
+      title +
+      "')" +
+      `> <p dir="rtl" >כותרת הסיפור: ${title} </p> <p dir="rtl" >תוכן הסיפור: ${story} </p> </button></div>`;
+
+    $("#delete_stories").append(str);
+  });
+}
+
+// delete story from realtime database firebase
+function delete_story(storyName) {
+  if (confirm("האם אתה בטוח שברצונך למחוק סיפור זה?")) {
+    firebase.database().ref("Stories").child(storyName).remove();
+
+    // reset div
+    let div = document.getElementById("delete_stories");
+    while (div.firstChild) {
+      div.removeChild(div.firstChild);
+    }
+  } else {
+    return;
+  }
+}
+
 // logout admin
 function logout() {
   // var rootRef = firebase.database().ref();
--- ToothFairy web/js/index.js
+++ ToothFairy web/js/index.js
@@ -1,31 +1,30 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyC9_qCGYu6YdTVNCxGHnV8T0SETnGyo8Qs",
-    authDomain: "toothfairyweb-6be63.firebaseapp.com",
-    projectId: "toothfairyweb-6be63",
-    storageBucket: "toothfairyweb-6be63.appspot.com",
-    messagingSenderId: "854109357498",
-    appId: "1:854109357498:web:e25bc99d544db5b31bc983",
-    measurementId: "G-GTMX6JLZCJ",
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+  apiKey: "AIzaSyC9_qCGYu6YdTVNCxGHnV8T0SETnGyo8Qs",
+  authDomain: "toothfairyweb-6be63.firebaseapp.com",
+  projectId: "toothfairyweb-6be63",
+  storageBucket: "toothfairyweb-6be63.appspot.com",
+  messagingSenderId: "854109357498",
+  appId: "1:854109357498:web:e25bc99d544db5b31bc983",
+  measurementId: "G-GTMX6JLZCJ",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
+// get all images from the firebase
+function getImages() {
+  let rootref = firebase.database().ref().child("Pictures");
 
-  // get all images from the firebase
-  function getImages() {
-    let rootref = firebase.database().ref().child("Pictures");
-
-    rootref.on("child_added", (snap) => {
-      let image = snap.child("Link").val();
-      let str
+  rootref.on("child_added", (snap) => {
+    let image = snap.child("Link").val();
+    let str;
     //   alert("image: " + image)
-      str = `<div class="mySlides"> <img src= ${image} style="width:40%" style="height:40%"></img></div>`
+    str = `<div class="mySlides"> <img src= ${image} style="width:40%" style="height:40%"></img></div>`;
 
-      $("#pictures").append(str);
-      plusSlides(1)
-    });
-  }
+    $("#pictures").append(str);
+    plusSlides(1);
+  });
+}
 
 //------ make the gallary  ---------//
 var slideIndex = 1;
@@ -33,12 +32,12 @@ showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 // slide images
@@ -47,16 +46,96 @@ function showSlides(n) {
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demo");
   var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
 }
 //------ end make the gallary  ---------//
+
+// get all Stories from the firebase and show the admin the Stories to delete
+function getStories() {
+  // run on all the data in the realtime database in filde Pictures
+  let rootref = firebase.database().ref().child("Stories");
+  rootref.on("child_added", (snap) => {
+    let title = snap.child("story_title").val();
+    let story = snap.child("story_content").val();
+
+    let str = `                                                    
+    <div class="swiper-slide">
+        <div class="card">
+            <img class="card-image" src="images/testimonial-1.svg"
+                alt="alternative">
+            <div class="card-body">
+                <p class="testimonial-author" dir="rtl">${title}</p>
+                <p class="testimonial-text" dir="rtl">${story}</p>
+            </div>
+        </div>
+    </div>
+    `;
+    $(".swiper-wrapper").append(str);
+
+    /* Image Slider - Swiper */
+    var imageSlider = new Swiper(".image-slider", {
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+      loop: true,
+      spaceBetween: 30,
+      slidesPerView: 5,
+      breakpoints: {
+        // when window is <= 580px
+        580: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        // when window is <= 768px
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        // when window is <= 992px
+        992: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        // when window is <= 1200px
+        1200: {
+          slidesPerView: 4,
+          spaceBetween: 20,
+        },
+      },
+    });
+
+    /* Card Slider - Swiper */
+    var cardSlider = new Swiper(".card-slider", {
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+      loop: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  });
+}
+
+function getImagesAndStories() {
+  getStories();
+  getImages();
+}
+
